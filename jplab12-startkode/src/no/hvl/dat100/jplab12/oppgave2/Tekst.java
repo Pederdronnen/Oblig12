5a4512c342ef7e610ce2edc97b9f50fe05ed18cb
Peder Drønnen <pedergd@hotmail.com>
2019-11-11 18:27:15 +0100

    Oppg12

---
+++ jplab12-startkode/src/no/hvl/dat100/jplab12/oppgave2/Tekst.java
@@ -0,0 +1,43 @@
+package no.hvl.dat100.jplab12.oppgave2;
+
+import no.hvl.dat100.jplab12.oppgave1.*;
+import no.hvl.dat100.jplab12.common.TODO;
+
+public class Tekst extends Innlegg {
+
+	private String tekst;
+	
+	public Tekst(int id, String bruker, String dato, String tekst) {
+		this(id, bruker, dato, 0, tekst);
+	}
+	
+	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
+		super(id, bruker, dato, likes);
+		this.tekst = tekst;
+	}
+	
+	public String getTekst() {
+		return tekst;
+	}
+	
+	public void setTekst(String tekst) {
+		this.tekst = tekst;
+	}
+
+	@Override
+	public String toString() {
+		String type = "TEKST\n";
+		if (this instanceof Bilde)
+				type = "BILDE\n";
+		
+		return type + super.toString() + tekst + "\n";
+
+	}
+	
+	// Metoden nedenfor er kun for valgfri oppgave 6
+	public String toHTML() {
+			
+		throw new UnsupportedOperationException(TODO.method());
+				
+	}
+}
---
+++ jplab12-startkode/src/no/hvl/dat100/jplab12/oppgave4/SkrivBlogg.java
@@ -0,0 +1,28 @@
+package no.hvl.dat100.jplab12.oppgave4;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import no.hvl.dat100.jplab12.oppgave3.*;
+import no.hvl.dat100.jplab12.common.TODO;
+
+
+public class SkrivBlogg {
+
+	private static String MAPPE = System.getProperty("user.dir") + "/src/no/hvl/dat100/jplab12/tests/";
+
+	public static boolean skriv(Blogg blogg, String filnavn) {
+
+		try {
+			var writer = new PrintWriter(MAPPE +filnavn);
+			
+			writer.println(blogg.toString());
+			writer.close();
+			return true;
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("Finner ikke filen" + e.getMessage());
+		}
+			return false;
+	}
+}
